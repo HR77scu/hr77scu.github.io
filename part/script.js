076f01ffd2ce26d3a2f8e022b2d7b40bb00f3848
HR77scu <rafiasik77@gmail.com>
2024-05-26 15:20:32 +0700

    update

--- part/script.js
+++ part/script.js
@@ -1,37 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // JSON data
-    const data = [
-        {
-            "image": "img/1.png",
-            "text": "WEb Pena kartini"
-        },
-        {
-            "image": "img/2.png",
-            "text": "System Loundry"
-        },
-        {
-            "image": "img/3.png",
-            "text": "SIM carting mobil"
-        },
-        {
-            "image": "img/4.png",
-            "text": "project Point of sales"
-        },
-        {
-            "image": "img/5.png",
-            "text": "Web desa jugo"
-        },
-        {
-            "image": "img/6.png",
-            "text": "PPDB Sekolah"
-        }
-    ];
+const galeriProject = [
+    { "image": "img/1.png", "text": "Project 1", "url": "https://example.com/project1" },
+    { "image": "img/2.png", "text": "Project 2", "url": "https://example.com/project2" },
+    { "image": "img/3.png", "text": "Project 3", "url": "https://example.com/project3" },
+    { "image": "img/4.png", "text": "Project 4", "url": "https://example.com/project4" },
+    { "image": "img/5.png", "text": "Project 5", "url": "https://example.com/project5" },
+    { "image": "img/6.png", "text": "Project 6", "url": "https://example.com/project6" }
+];
 
-    // Insert portfolio items into the HTML
+document.addEventListener('DOMContentLoaded', function() {
     const portfolioContainer = document.getElementById('portfolio-items');
     let portfolioHTML = '';
 
-    data.forEach(item => {
+    galeriProject.forEach(item => {
         portfolioHTML += `
             <div class="col-md-4">
                 <div class="card">
@@ -45,4 +25,28 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     portfolioContainer.innerHTML = portfolioHTML;
+
 });
+document.addEventListener('DOMContentLoaded', function(){
+    const galleryContainer = document.getElementById('gallery');
+    let galleryHTML = '';
+    
+    galeriProject.forEach(item => {
+        galleryHTML += `
+            <div class="gallery-item">
+                <img src="${item.image}" alt="${item.text}">
+                <p><a href="${item.url}" target="_blank">${item.text}</a></p>
+            </div>
+        `;
+    });        
+    galleryContainer.innerHTML = galleryHTML;
+});
+
+function sendWhatsAppMessage() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var no_telp = document.getElementById('no_telp').value;
+    var pesan = document.getElementById('pesan').value;
+    var url = `https://wa.me/6285712498265?text=Nama: ${name}%0AEmail: ${email}%0ANo Telephone: ${no_telp}%0APesan: ${pesan}`;
+    window.open(url, '_blank');
+}
